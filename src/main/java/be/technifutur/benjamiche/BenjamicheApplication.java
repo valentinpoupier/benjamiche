fc0e7cb04527ffe8390b5b895b5f90ad09b9eb00
Valentin Poupier <poupier_valentin@hotmail.com>
2023-02-07 08:51:52 +0100

    Initial commit

---
+++ src/main/java/be/technifutur/benjamiche/BenjamicheApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.benjamiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenjamicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenjamicheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/benjamiche/BenjamicheApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.benjamiche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenjamicheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
